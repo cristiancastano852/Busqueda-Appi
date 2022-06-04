9b3004414eea6d264e02e85d5960f22bae775b49
Cristian Castaño <cristian.castano1@udea.edu.co>
2022-06-04 16:57:17 -0500

    Add: Item

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="hello">
-    <input type="text" v-model="query" placeholder="Ingrese lo que deseea buscar"> <br>
-    <button @click="search()" class="ImgButton">Buscar imagen</button>
+<div>
+  <div class="list" v-if="bool">
+  <h1> Cristian Castaño </h1>
+    <input type="text" v-model="query" v-on:keyup.enter="search()" placeholder="Ingrese lo que deseea buscar"> <br>
+    <button @click="search()" class="ImgButton">Buscar</button>
     
       <div class="imgResults">
           <div v-for="item in items" :key="item.title" class="imgResults">
             <h1>Producto: {{item.title}}</h1>
+            <img :src="item.thumbnail">
             <h2>Precio: {{item.price}}</h2>
             <button variant="dark" href="#" @click="SeeProduct(item.id)" >Mostrar</button>
           </div>
       </div>
   </div>
+  <div class="product" v-if="bool==false">
+    <h1>{{item.title}}</h1>
+    <img :src="item.pictures[0].secure_url">
+    <h2>Precio: {{item.price}}  {{item.currency_id}}</h2>
+    <h3>{{item.warranty}}</h3>
+     <h3>Estado: {{item.condition}}</h3>
+    <h3> </h3>
+    <button variant="dark" href="#" @click="search()" >Volver</button>
+    <h3> </h3>
+  </div>
+</div>
 </template>
 
 <script>
@@ -22,10 +36,12 @@ export default {
       query:'',
       item: null,
       items : null,
+      bool: true,
     }
   },
   methods : {
      async search(){
+       this.bool=true
        let data = await axios.get('https://api.mercadolibre.com/sites/MCO/search?q='+this.query)
         .then( res=>{
           return res.data.results;
@@ -34,8 +50,10 @@ export default {
         this.items=data;
         console.log(data);
      },
-      SeeProduct(id){
-       let data = axios.get('https://api.mercadolibre.com/items/'+id)
+
+      async SeeProduct(id){
+        this.bool=false;
+       let data = await axios.get('https://api.mercadolibre.com/items/'+id)
         .then( res=>{
           return res.data;
         })
